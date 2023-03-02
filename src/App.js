41de51e0fcac4ff05da05ab11fc2b8f27ffda068
Marie Lewis <mariea1022@gmail.com>
2023-03-02 00:23:59 -0800

    added react-router-dom and tried to implement it to navigate between the pages

--- src/App.js
+++ src/App.js
@@ -1,14 +1,62 @@
-import './App.css';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import Project from './components/Project';
+import "./App.css";
+// import { BrowserRouter as Router, Switch,
+//   Route, Redirect,} from "react-router-dom";
+
+// import AboutMe from "./components/AboutMe";
+// import Projects from "./components/Projects"
+// import Contact from "./components/Contact"
+// import Resume from "./components/Resume";
+
+// import Header from "./components/Header";
+// import Footer from "./components/Footer";
+// import Project from "./components/Project";
+import PortfolioContainer from "./components/PortfolioContainer";
+
+// function App() {
+//   return (
+//     <>
+//       {/* This is the alias of BrowserRouter i.e. Router */}
+//       <Router>
+//         <Switch>
+//           {/* This route is for home component
+//           with exact path "/", in component props
+//           we passes the imported component*/}
+//           <Route exact path="/" component={Homepage} />
+
+//           {/* This route is for aboutme component
+//           with exact path "/aboutme", in component
+//           props we passes the imported component*/}
+//           <Route path="/aboutme" component={AboutMe} />
+
+//           {/* This route is for projects component
+//           with exact path "/projects", in component
+//           props we passes the imported component*/}
+//           <Route path="/projects" component={Projects} />
+
+//           {/* This route is for contact component
+//           with exact path "/contact", in
+//           component props we passes the imported component*/}
+//           <Route path="/contact" component={Contact} />
+
+//           {/* This route is for resume component
+//           with exact path "/resume", in
+//           component props we passes the imported component*/}
+//           <Route path="/resume" component={Resume} />
+
+//           {/* If any route mismatches the upper
+//           route endpoints then, redirect triggers
+//           and redirects app to home component with to="/" */}
+//           <Redirect to="/" />
+//         </Switch>
+//       </Router>
+//     </>
+//   );
+// }
 
 function App() {
   return (
-    <div className=''>
-      <Header />
-      <Project />
-      <Footer />
+    <div>
+      <PortfolioContainer />
     </div>
   );
 }
--- src/components/Project.js
+++ src/components/Project.js
@@ -1,21 +1,52 @@
 import React from "react";
+// import { BrowserRouter as Router, Switch,
+//   Route, Redirect,} from "react-router-dom";
+import AboutMe from "./pages/AboutMe";
+// import Projects from "./Projects"
+// import Contact from "./Contact"
+// import Resume from "./Resume";
+
 
 function Project() {
   return (
-    <div
-      className="Project h-screen flex place-items-center p-8 bg-local bg-gray-500 bg-center bg-no-repeat bg-cover bg-blend-multiply"
-      style={{
-        backgroundImage: `url("https://static.vecteezy.com/system/resources/previews/000/227/854/original/female-developer-vector.jpg")`,
-      }}
-    >
-<div className="w-full text-center">
-        <p className="text-4xl mb-5 font-bold leading-tight tracking-tight text-white underline decoration-sky-500">
-          Hi there! 👋  My name is Marie Lewis.
-        </p>
-        <p className="text-2xl mb-5 font-bold leading-tight tracking-tight text-white underline decoration-pink-500">
-          Recruiter by day, full stack dev by night, and foodie for life!
-        </p>
-        </div>
+    //     <>
+    //   {/* This is the alias of BrowserRouter i.e. Router */}
+    //   <Router>
+    //     <Switch>
+    //       {/* This route is for home component
+    //       with exact path "/", in component props
+    //       we passes the imported component*/}
+    //       <Route exact path="/" component={Homepage} />
+
+    //       {/* This route is for aboutme component
+    //       with exact path "/aboutme", in component
+    //       props we passes the imported component*/}
+    //       <Route path="/aboutme" component={AboutMe} />
+
+    //       {/* This route is for projects component
+    //       with exact path "/projects", in component
+    //       props we passes the imported component*/}
+    //       <Route path="/projects" component={Projects} />
+
+    //       {/* This route is for contact component
+    //       with exact path "/contact", in
+    //       component props we passes the imported component*/}
+    //       <Route path="/contact" component={Contact} />
+
+    //       {/* This route is for resume component
+    //       with exact path "/resume", in
+    //       component props we passes the imported component*/}
+    //       <Route path="/resume" component={Resume} />
+
+    //       {/* If any route mismatches the upper
+    //       route endpoints then, redirect triggers
+    //       and redirects app to home component with to="/" */}
+    //       <Redirect to="/" />
+    //     </Switch>
+    //   </Router>
+    // </>
+    <div>
+      <AboutMe />
     </div>
   );
 }
