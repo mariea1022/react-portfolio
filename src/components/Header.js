5a6afa1c46d7b4f5b2682a1241420308fe6e2569
Marie Lewis <mariea1022@gmail.com>
2023-03-02 00:22:38 -0800

    moved header content to Navigation component so Navigation contains js for my name + nav bar

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,16 +3,7 @@ import Navigation from "./Navigation";
 
 function Header() {
   return (
-    <div className="bg-white border-gray-200 px-2 sm:px-4 py-2.5 dark:bg-gray-900">
-      <div className="container flex flex-wrap items-center justify-between mx-auto">
-        <a href="https://" class="flex items-center">
-          <div className="font-medium dark:text-white">
-            <div>Marie Lewis</div>
-          </div>
-        </a>
         <Navigation></Navigation>
-      </div>
-    </div>
   );
 }
 
