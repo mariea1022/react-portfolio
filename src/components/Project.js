7594eeb20b0527ed59f8ca32272d25be3e07b5b5
Marie Lewis <mariea1022@gmail.com>
2023-03-02 20:39:33 -0800

    removed react-router-dom since this a SAP

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,52 +1,20 @@
 import React from "react";
-// import { BrowserRouter as Router, Switch,
-//   Route, Redirect,} from "react-router-dom";
-import AboutMe from "./pages/AboutMe";
-// import Projects from "./Projects"
-// import Contact from "./Contact"
-// import Resume from "./Resume";
 
-
-function Project() {
+function Project(props) {
+  console.log(props)
   return (
-    //     <>
-    //   {/* This is the alias of BrowserRouter i.e. Router */}
-    //   <Router>
-    //     <Switch>
-    //       {/* This route is for home component
-    //       with exact path "/", in component props
-    //       we passes the imported component*/}
-    //       <Route exact path="/" component={Homepage} />
-
-    //       {/* This route is for aboutme component
-    //       with exact path "/aboutme", in component
-    //       props we passes the imported component*/}
-    //       <Route path="/aboutme" component={AboutMe} />
-
-    //       {/* This route is for projects component
-    //       with exact path "/projects", in component
-    //       props we passes the imported component*/}
-    //       <Route path="/projects" component={Projects} />
-
-    //       {/* This route is for contact component
-    //       with exact path "/contact", in
-    //       component props we passes the imported component*/}
-    //       <Route path="/contact" component={Contact} />
-
-    //       {/* This route is for resume component
-    //       with exact path "/resume", in
-    //       component props we passes the imported component*/}
-    //       <Route path="/resume" component={Resume} />
-
-    //       {/* If any route mismatches the upper
-    //       route endpoints then, redirect triggers
-    //       and redirects app to home component with to="/" */}
-    //       <Redirect to="/" />
-    //     </Switch>
-    //   </Router>
-    // </>
-    <div>
-      <AboutMe />
+    <div className="projects-card">
+      <div className="image-card" id={props.id}></div>
+      <div className="information-card">
+        <h5>{props.name}</h5>
+        <small>
+          <a href={props.github}>Github</a>
+        </small>
+        <br />
+        <small>
+          <a href={props.website}>Website</a>
+        </small>
+      </div>
     </div>
   );
 }
